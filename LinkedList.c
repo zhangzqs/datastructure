8d96ce3f6839e36746c5c384fc5e61922c0f95b4
zzq <i@zhangzqs.cn>
2022-06-25 10:12:51 +0800

    add: 添加定位元素函数

--- LinkedList.c
+++ LinkedList.c
@@ -17,7 +17,7 @@ LinkedList NewLinkedList() {
 }
 
 // 在某结点后插入新结点元素
-Node *InsertAfter(Node *node, ElemType e){
+Node *InsertAfter(Node *node, ElemType e) {
     Node *n = malloc(sizeof(Node));
     n->data = e;
     n->next = node->next;
@@ -26,57 +26,65 @@ Node *InsertAfter(Node *node, ElemType e){
 }
 
 // 在某结点后以头插法建立单链表
-void HeadInsert(Node* node, ElemType *arr, int size) {
-    for(int i=0;i<size;i++){
+void HeadInsert(Node *node, ElemType *arr, int size) {
+    for (int i = 0; i < size; i++) {
         InsertAfter(node, arr[i]);
     }
 }
 
 // 在某结点后以尾插法建立单链表
-void TailInsert(Node* node, ElemType *arr, int size) {
+void TailInsert(Node *node, ElemType *arr, int size) {
     // 指向尾结点的指针
     Node *r = node;
-    for(int i=0;i<size;i++){
+    for (int i = 0; i < size; i++) {
         r = InsertAfter(r, arr[i]);
     }
 }
 
-Node *GetElem(LinkedList headNode, int i){
+Node *GetElem(LinkedList L, int i) {
     if (i < 0) return NULL;
 
-    Node *n = headNode;
+    Node *n = L;
     int j = 0;
     // 当i=3时候
     // j可以取到0, 1, 2，始终小于i=3
     // 即仅当j<i时，才继续循环
-    while(n != NULL & j < i) {
+    while (n != NULL && j < i) {
         n = n->next;
         j++;
     }
     return n;
 }
 
-void PrintList(LinkedList headNode) {
-    Node *n = headNode->next;
+void PrintList(LinkedList L) {
+    Node *n = L->next;
     printf("[");
-    while(n != NULL) {
+    while (n != NULL) {
         printf("%d, ", n->data);
         n = n->next;
     }
     printf("]\n");
 }
 
+Node *LocateElem(LinkedList L, ElemType e) {
+    Node *p = L->next;
+    while (p != NULL && p->data != e) {
+        p = p->next;
+    }
+    return p;
+}
+
 int main() {
-    int arr[] = {1,2,3,4,5};
+    int arr[] = {1, 2, 3, 4, 5};
     LinkedList L = NewLinkedList();
-    HeadInsert(L, arr, sizeof(arr)/sizeof(int));
-    HeadInsert(L, arr, sizeof(arr)/sizeof(int));
+    HeadInsert(L, arr, sizeof(arr) / sizeof(int));
+    HeadInsert(L, arr, sizeof(arr) / sizeof(int));
     PrintList(L);
-    TailInsert(L, arr, sizeof(arr)/sizeof(int));
-    TailInsert(L, arr, sizeof(arr)/sizeof(int));
+    TailInsert(L, arr, sizeof(arr) / sizeof(int));
+    TailInsert(L, arr, sizeof(arr) / sizeof(int));
     PrintList(L);
 
-    Node *node = GetElem(L,6);
+    Node *node = GetElem(L, 6);
     printf("%d\n", node->data);
     return 0;
 }
